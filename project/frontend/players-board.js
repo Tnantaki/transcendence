3c4d82a4439f97e775b04a36af1f250b3cd5d1e9
Thanagorn Ruangsit <thanagorn1611@gmail.com>
2024-06-09 13:32:42 +0700

    fixed: addBtn now move position after new playerboard is drawn / long player names no longers go further the area of the playerboard

---
+++ project/frontend/players-board.js
@@ -0,0 +1,107 @@
+const BUTTON_RADIUS = 10;
+const BUTTON_SPACE = 40;
+const BOARD_MARGIN = 505;
+
+// create dynamic list view
+var players = [{name: "Player 1"}];
+function drawPlayerList(players)
+{
+	const startX = canvas.width - 950;
+	const startY = 100;
+	const padding = 30;
+	const textPadding = 40;
+	const lineHeight = 40;
+	const maxWidth = 450;
+
+	// draw players board
+	const boardColor = "rgb(255, 255, 255, 0.2)";
+	ctx.fillStyle = boardColor;
+	ctx.roundRect(startX, startY, maxWidth, lineHeight * players.length + padding * 2, 10);
+	ctx.fill();
+
+	ctx.font = "40px Irish Grover";
+	ctx.fillStyle = "white";
+	ctx.textBaseline = "top";
+	// ctx.textAlign = "start";
+	ctx.fillText("Players", startX + 50, startY - 23);
+
+	// write players' names
+	ctx.font = "25px Irish Grover";
+	ctx.fillStyle = "white";
+	ctx.textBaseline = "top";
+	ctx.textAlign = "start";
+	players.forEach((player, index) => {
+		const yPos = startY + padding + index * lineHeight;
+		ctx.fillText(player.name, startX + textPadding, yPos + 20);
+	});
+}
+
+function addPlayer(name)
+{
+	players.push({name: name});
+	drawNewCanvas(players);
+}
+
+function drawCircle(x, y, radius, fillStyle)
+{
+	ctx.beginPath();
+	ctx.arc(x, y, radius, 0, 2 * Math.PI);
+	ctx.fillStyle = fillStyle;
+	ctx.fill();
+
+	// highlight the button
+	// ctx.strokeStyle = "white";
+	// ctx.stroke();
+}
+
+function drawAddBtn(btnY)
+{
+	const btnX = canvas.width - BOARD_MARGIN;
+	drawCircle(btnX, btnY, BUTTON_RADIUS, "rgb(52, 65, 172)");
+
+	ctx.lineWidth = 3;
+	ctx.beginPath();
+	ctx.strokeStyle = "white";
+	ctx.moveTo(btnX + BUTTON_RADIUS, btnY);
+	ctx.lineTo(btnX - BUTTON_RADIUS, btnY);
+	ctx.stroke();
+
+	// vertical
+	ctx.beginPath();
+	ctx.strokeStyle = "white";
+	ctx.moveTo(btnX, btnY - BUTTON_RADIUS);
+	ctx.lineTo(btnX, btnY + BUTTON_RADIUS);
+	ctx.stroke();
+}
+
+function initAddBtn()
+{
+	let initBtnY = 200;
+	if (players.length > 1)
+		initBtnY += (players.length - 1) * BUTTON_SPACE;
+	drawAddBtn(initBtnY);
+}
+
+function isAddButton(circleX, circleY, radius, clickX, clickY)
+{
+	const dx = circleX - clickX;
+	const dy = circleY - clickY;
+	const dis = Math.sqrt(dx * dx + dy * dy);
+	return dis <= radius;
+}
+
+canvas.addEventListener("click", function(event) {
+	const rect = canvas.getBoundingClientRect();
+	const x = event.clientX - rect.left;
+	const y = event.clientY - rect.top;
+
+	const btnX = canvas.width - 505;
+	const btnY = 200;
+	const btnRadius = 10;
+
+	if (isAddButton(btnX, btnY, btnRadius, x, y)) {
+		const newName = prompt("Enter player name:");
+		if (newName)
+			addPlayer(newName);
+	}
+});
--- project/frontend/tournament-menu.js
+++ project/frontend/tournament-menu.js
@@ -8,13 +8,14 @@ let imageY = 0;
 let pongImg = new Image();
 pongImg.src = "images/table-tennis.png";
 pongImg.onload = function() { // call the anonymous function when the image is loaded
-	drawCanvas();
+	initCanvas();
 	drawStartBtn(imageX, imageY);
 	drawPlayerList(players);
-	drawAddBtn();
+	initAddBtn();
+	// drawAddBtn();
 };
 
-function drawCanvas()
+function initCanvas()
 {
 	// clear canvas
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -42,14 +43,12 @@ function drawCanvas()
 	// console.log("canvas: " + imageX + " " + imageY)
 }
 
-// button setup
-const btnWidth = 200;
-const btnHeight = 50;
-
 function drawStartBtn()
 {
 	const x = imageX + 50;
 	const y = imageY + 350;
+	const btnWidth = 200;
+	const btnHeight = 50;
 
 	// draw button background
 	ctx.fillStyle = "white";
@@ -65,100 +64,11 @@ function drawStartBtn()
 
 // listen to start button click
 
-
-// create dynamic list view
-let players = [{name: "Player 1"}];
-function drawPlayerList(players)
-{
-	const startX = canvas.width - 950;
-	const startY = 100;
-	const padding = 30;
-	const lineHeight = 40;
-	const maxWidth = 450;
-
-
-	// draw players board
-	ctx.fillStyle = "rgb(255, 255, 255, 0.2)";
-	ctx.roundRect(startX, startY, maxWidth, lineHeight * players.length + padding * 2, 10);
-	ctx.fill();
-
-	ctx.font = "40px Irish Grover";
-	ctx.fillStyle = "white";
-	ctx.textBaseline = "top";
-	ctx.fillText("Players", startX + 50, startY - 23);
-
-	// write players' names
-	ctx.font = "25px Irish Grover";
-	ctx.fillStyle = "white";
-	ctx.textBaseline = "top";
-	players.forEach((player, index) => {
-		const yPos = startY + padding + index * lineHeight;
-		ctx.fillText(player.name, startX + padding + 40, yPos + 20);
-	});
-}
-
-function drawNewCanvas()
+function drawNewCanvas(players)
 {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	drawCanvas();
-	drawPlayerList();
+	initCanvas();
+	drawPlayerList(players);
+	drawStartBtn(imageX, imageY);
+	initAddBtn();
 }
-
-function addPlayer(name)
-{
-	players.push({name: name});
-	drawNewCanvas(players);
-}
-
-function drawAddBtn()
-{
-	const btnX = canvas.width - 505;
-	const btnY = 200;
-	const btnRadius = 10;
-
-	ctx.beginPath();
-	ctx.arc(btnX, btnY, btnRadius, 0, 2 * Math.PI);
-	ctx.fillStyle = "rgb(52, 65, 172)";
-	ctx.fill();
-	// ctx.strokeStyle = "white";
-	// ctx.stroke();
-
-	// draw + on btn
-	ctx.lineWidth = 3;
-	ctx.beginPath();
-	ctx.strokeStyle = "white";
-	ctx.moveTo(btnX + 10, btnY);
-	ctx.lineTo(btnX - 10, btnY);
-	ctx.stroke();
-
-	ctx.beginPath();
-	ctx.linewidth = 2;
-	ctx.strokeStyle = "white";
-	ctx.moveTo(btnX, btnY - 10);
-	ctx.lineTo(btnX, 210);
-	ctx.stroke();
-}
-
-function isAddButton(circleX, circleY, radius, clickX, clickY)
-{
-	const dx = circleX - clickX;
-	const dy = circleY - clickY;
-	const dis = Math.sqrt(dx * dx + dy * dy);
-	return dis <= radius;
-}
-
-canvas.addEventListener("click", function(event) {
-	const rect = canvas.getBoundingClientRect();
-	const x = event.clientX - rect.left;
-	const y = event.clientY - rect.top;
-
-	const btnX = canvas.width - 505;
-	const btnY = 200;
-	const btnRadius = 10;
-
-	if (isAddButton(btnX, btnY, btnRadius, x, y)) {
-		const newName = prompt("Enter player name:");
-		if (newName)
-			addPlayer(newName);
-	}
-});
