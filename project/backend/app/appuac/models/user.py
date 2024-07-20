33dbc6069bf8486932dfbdcb1b0783733af88ece
narin <narin.w@lexnetix.com>
2024-07-20 19:22:42 +0700

    is_online

--- project/backend/app/appuac/models/user.py
+++ project/backend/app/appuac/models/user.py
@@ -5,10 +5,12 @@ from appuac.models.base import (
     BaseAutoDate,
     BaseID,
 )
-
+from datetime import timedelta
+from django.utils import timezone
 
 def gen_id():
     return generate(size=24)
+
 
 class User(AbstractUser):
     id = models.CharField(
@@ -22,10 +24,14 @@ class User(AbstractUser):
     updated = models.DateTimeField(auto_now=True)
     friend = models.ManyToManyField("User", related_name="myfriend")
 
+    @property
+    def is_online(self):
+        res = self.authsession_set.filter(last_used__gte=timezone.now() - timedelta(minutes=60))
+        return res.exists()
 
     class Meta:
         db_table = "auth_user"
-    
+
     def __repr__(self) -> str:
         return f"user: {self.id}"
 
@@ -34,6 +40,14 @@ class User(AbstractUser):
 
 
 class FriendRequest(BaseAutoDate, BaseID):
-    requestor = models.ForeignKey(User, related_name="friend_requestor", on_delete=models.CASCADE,)
-    receiver = models.ForeignKey(User, related_name="friend_reciever", on_delete=models.CASCADE,)
+    requestor = models.ForeignKey(
+        User,
+        related_name="friend_requestor",
+        on_delete=models.CASCADE,
+    )
+    receiver = models.ForeignKey(
+        User,
+        related_name="friend_reciever",
+        on_delete=models.CASCADE,
+    )
     status = models.CharField(max_length=255, default="PENDING")
--- project/backend/app/appuac/routes/user.py
+++ project/backend/app/appuac/routes/user.py
@@ -1,6 +1,7 @@
 from ninja import Router, Path, Field
 from ninja.errors import HttpError
 from appuac.models.user import User, FriendRequest
+from appuac.models.authsession import AuthSession
 from ninja import Schema, ModelSchema
 from pydantic import field_validator
 from django.shortcuts import get_object_or_404
@@ -13,6 +14,8 @@ class SimpleRespone(Schema):
 
 
 class UserSchema(ModelSchema):
+    is_online: bool = Field(default=False)
+
     class Meta:
         model = User
         fields = [
@@ -261,7 +264,7 @@ def get_friend_list(request):
     return user.friend
 
 @router.delete(
-    "/friend/{user_id}/",
+    "/friend/{friend_id}/",
     response={
         204: None,
     }
